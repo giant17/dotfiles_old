5bec22524342d0bd6621619bb062e383a112fe95
Gianluca <giantadori@gmail.com>
2019-08-31 15:34:32 +0200

    update

---
+++ newsboat/scripts/newsboat/yt.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+error() { notify-send " ERROR" "$1"; exit 1;}
+
+url=$1
+
+while getopts 'avp:' OPTION; do
+  case "$OPTION" in
+    a)
+
+      FILENAME=$(youtube-dl --get-filename -o "%(title)s." $url)
+	  tsp notify-send "🎵 Download started." "$FILENAME"
+	  tsp youtube-dl --add-metadata -ic -f bestaudio/best -o '~/%(upload_date)s-%(title)s.%(ext)s' $url
+	  tsp notify-send "🎵 Download complete." "$FILENAME"
+      ;;
+
+
+    v)
+      FILENAME=$(youtube-dl --get-filename -o "%(title)s." $url)
+	  tsp notify-send " Download started." "$FILENAME"
+	  tsp youtube-dl --add-metadata -ic -o '~/%(upload_date)s-%(title)s' $url
+	  tsp notify-send " Downloading complete." "$FILENAME"
+	  ;;
+
+    p)
+      FILENAME=$(youtube-dl --get-filename -o "%(playlist)s." $url)
+	  tsp notify-send " Download playlist started." "$FILENAME"
+	  tsp youtube-dl --add-metadata -ic -o '~/downloads/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' $url
+	  tsp notify-send " Downloading complete." "$FILENAME"
+      ;;
+	*)
+	  error "Please choose a valid option."
+  esac
+done
+
