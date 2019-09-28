416ad2be7eed9589da9d7ebc623c7ba6c1169fe3
Gianluca <giantadori@gmail.com>
2019-09-28 15:29:30 +0200

    Scripts clean

--- cron/.local/bin/cron/info.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Display notify info
-
-battery=$(cat /sys/class/power_supply/BAT0/capacity)
-time=$(date '+%H:%M')
-cal=$(calcurse -D ~/.config/calcurse -a | awk '/^ /' | wc -l)
-
-notify-send " $time" " $battery\\n$cal"
--- tmux/.local/bin/INBOX/tmuxTerm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-tmux attach -t scratchterm || tmux new-session -t scratchterm
