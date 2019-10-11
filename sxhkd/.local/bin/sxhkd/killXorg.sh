8a5dd05aa14748ac32ae79c39c53fadea5d44acb
Gianluca <giantadori@gmail.com>
2019-10-11 15:07:31 +0200

    Update

--- sxhkd/.local/bin/sxhkd/killXorg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Dmenu prompt for quitting Xorg
-
-prompt() { [ "$(printf "No\nYes" | dmenu.sh -i -p "$1")" = "Yes" ] && $2 ;}
-prompt "Quitting Xorg?" "killall Xorg"
--- sxhkd/.local/bin/sxhkd/prompt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# A dmenu binary prompt script.
-# Gives a dmenu prompt labeled with $1 to perform command $2.
-
-ifinstalled rofi
-[ "$(printf "No\\nYes" | rofi -dmenu -i -p "$1")" = "Yes" ] && $2
